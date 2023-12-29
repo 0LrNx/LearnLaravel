364d8dff764df3800c4ef32e9cfc08e0914adca3
FI$$ <FISSSSSS@proton.me>
2023-12-29 22:33:20 +0100

    feat : ORM Eloquent

--- MonApp/routes/web.php
+++ MonApp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -22,17 +23,39 @@ Route::get('/', function () {
 
 Route::prefix('/blog')->name('blog.')->group(function () {
     Route::get('/', function (Request $request) {
+
+        /* === ORM ELOQUENT === */
+
+        //$posts = Post::paginate(1, ['id', 'title']);
+        /*$post = Post::create([
+            'title' => 'Mon nouveau titre',
+            'slug' => 'nouveau-titre-test',
+            'content' => 'Nouveau contenu'
+        ]);
+
+        dd($post);
+        return $post;*/
+
+        return $post = Post::paginate(25);
+
+        /*
         return [
             "link" => \route('blog.show', ['slug' => 'article', 'id' => 13]),
-        ];
+        ];*/
     })->name('index');
 
-    Route::get('/{slug}/{id}', function (string $slug, string $id,Request $request) {
-        return [
+    Route::get('/{slug}-{id}', function (string $slug, string $id,Request $request) {
+        $post = Post::findOrFail($id);
+        // redirection automatique si le slug n'est pas correcte.
+        if($post -> slug !== $slug){
+            return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
+        }
+        return $post;
+        /*return [
             'slug' => $slug,
             'id' => $id,
             'name' => $request->input('name')
-        ];
+        ];*/
     })->where([
         'slug' => '[a-z0-9\-]+',
         'id' => '[0-9]+'
