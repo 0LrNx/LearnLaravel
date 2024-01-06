2b933251fb70d24e698b7e100d1d1ed692af85ae
FI$$ <FISSSSSS@proton.me>
2024-01-06 16:25:49 +0100

    feat : view -> blade

--- MonApp/app/Http/Controllers/BlogController.php
+++ MonApp/app/Http/Controllers/BlogController.php
@@ -6,21 +6,35 @@ use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
 
 class BlogController extends Controller
 {
 
-    public function index (): Paginator {
-        return Post::paginate(25);
+    public function index (): View {
+
+        $validator = Validator::make([
+            'title' => ''
+        ], [
+            'title' => 'required|min:8'
+        ]);
+        dd($validator->fails());
+
+        return view('blog.index', [
+            'posts' => Post::paginate(1)
+        ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | Post {
+    public function show(string $slug, string $id): RedirectResponse | View {
         $post = Post::findOrFail($id);
         // redirection automatique si le slug n'est pas correcte.
         if($post -> slug !== $slug){
             return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
         }
-        return $post; 
+        return view("blog.show", [
+            'post' => $post
+        ]); 
     }
 
 
---
+++ MonApp/resources/views/blog/index.blade.php
@@ -0,0 +1,18 @@
+@extends('base')
+
+@section('title', 'Accueil du blog')
+
+
+@section('content')
+    <h1>Mon Blog</h1>
+    
+    @foreach($posts as $post)
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->content }}</p>
+        <p>
+            <a href="{{ route('blog.show',['slug' => $post->slug, 'id' => $post->id])}}" class="btn btn-primary">Lire la suite</a>
+        </p>
+    @endforeach
+
+    {{ $posts->links() }}
+@endsection 
